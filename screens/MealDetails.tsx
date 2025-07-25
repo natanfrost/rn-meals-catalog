9ed0115941bbd82b5b20b41f948d04b0605cdc9e
Nataniel Richardt <nataniel.r.v@gmail.com>
2025-07-25 11:27:35 -0300

    refactor: Wrap MealDetails content in ScrollView for better layout

--- screens/MealDetails.tsx
+++ screens/MealDetails.tsx
@@ -1,6 +1,6 @@
 import { useNavigation, useRoute } from "@react-navigation/core";
 import { useLayoutEffect, useState } from "react";
-import { View, Text, StyleSheet, Image } from "react-native";
+import { View, Text, StyleSheet, Image, ScrollView } from "react-native";
 import { MEALS } from "../data/dummy-data";
 import Meal from "../models/meal";
 
@@ -21,30 +21,34 @@ const MealDetails = () => {
   }, [mealId]);
 
   return (
-    <View>
-      <Image source={{ uri: meal?.imageUrl }} style={styles.image} />
-      <Text style={styles.title}>{meal?.title}</Text>
-      <Text style={styles.details}>
-        {meal?.duration}m | {meal?.complexity.toUpperCase()} |{" "}
-        {meal?.affordability.toUpperCase()}
-      </Text>
-      <Text style={styles.subtitle}>Ingredients</Text>
+    <ScrollView>
       <View>
-        {meal?.ingredients.map((ingredient) => (
-          <Text key={ingredient} style={styles.ingredient}>
-            {ingredient}
-          </Text>
-        ))}
+        <Image source={{ uri: meal?.imageUrl }} style={styles.image} />
+        <Text style={styles.title}>{meal?.title}</Text>
+        <Text style={styles.details}>
+          {meal?.duration}m | {meal?.complexity.toUpperCase()} |{" "}
+          {meal?.affordability.toUpperCase()}
+        </Text>
+        <View style={styles.detailsContainer}>
+          <Text style={styles.subtitle}>Ingredients</Text>
+          <View>
+            {meal?.ingredients.map((ingredient) => (
+              <Text key={ingredient} style={styles.ingredient}>
+                {ingredient}
+              </Text>
+            ))}
+          </View>
+          <Text style={styles.subtitle}>Steps</Text>
+          <View>
+            {meal?.steps.map((step, index) => (
+              <Text key={index} style={styles.step}>
+                {index + 1}. {step}
+              </Text>
+            ))}
+          </View>
+        </View>
       </View>
-      <Text style={styles.subtitle}>Steps</Text>
-      <View>
-        {meal?.steps.map((step, index) => (
-          <Text key={index} style={styles.step}>
-            {index + 1}. {step}
-          </Text>
-        ))}
-      </View>
-    </View>
+    </ScrollView>
   );
 };
 
@@ -59,18 +63,21 @@ const styles = StyleSheet.create({
     fontSize: 24,
     fontWeight: "bold",
     textAlign: "center",
-    marginVertical: 10,
+  },
+  detailsContainer: {
+    marginHorizontal: 12,
   },
   details: {
     textAlign: "center",
     color: "#666",
-    marginBottom: 10,
+    margin: 8,
   },
   subtitle: {
     fontSize: 20,
     fontWeight: "bold",
     marginVertical: 10,
     marginLeft: 10,
+    textAlign: "center",
   },
   ingredient: {
     fontSize: 16,
