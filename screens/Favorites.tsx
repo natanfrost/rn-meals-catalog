14e02af58270754fb0ef436b63aebc7c23a428a7
Nataniel Richardt <nataniel.vieira@terceiro.rnp.br>
2025-07-25 17:45:35 -0300

    feat: Add conditional rendering for favorites in Favorites screen

--- screens/Favorites.tsx
+++ screens/Favorites.tsx
@@ -1,10 +1,31 @@
+import { StyleSheet, Text, View } from "react-native";
 import MealList from "../components/MealList/MealList";
 import { FavoritesContext } from "../store/context/favorites-context";
 import { useContext } from "react";
 
 const Favorites = () => {
   const { favorites } = useContext(FavoritesContext);
-  return <MealList categoryMeals={favorites} />;
+  return favorites.length > 0 ? (
+    <MealList categoryMeals={favorites} />
+  ) : (
+    <View style={styles.container}>
+      <Text style={styles.noDataText}>No favorites found.</Text>
+    </View>
+  );
 };
 
 export default Favorites;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#e4e2e2",
+  },
+  noDataText: {
+    textAlign: "center",
+    fontSize: 24,
+    color: "#666",
+  },
+});
